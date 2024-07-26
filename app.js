2d8a96d3b0b4ae01c9db774b60e085cc904f909e
Shreya <shreyasingh070801@gmail.com>
2024-07-26 22:32:53 +0530

    Request token and verify the token

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-require('dot.env').config()
+require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
 const app = express()
 
+const mainRouter = require('./routes/main')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.static('./public'))
 app.use(express.json())
 
-const port = process.env.PORT || 5000
+//to check in postman
+app.use('/api/v1', mainRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
 
 const start  = async () => {
     try{
---
+++ controllers/main.js
@@ -0,0 +1,49 @@
+// setup authentication so only the request with JWT can access the dashboard
+
+const jwt  = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+
+const login = async (req, res) =>{
+    const {username, password} = req.body
+    //mongo
+    //Joi package
+    // check in the controller
+
+    //3rd way
+    if(!username || !password)
+    {
+        throw new CustomAPIError('Please provide email and password', 400)
+    }
+
+    //for demo, normally provided by DB
+    const id = new Date().getDate()
+
+
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn:'30d'})
+    // console.log(username, password);
+    //res.send('Fake Login/Register/Signup Route')
+    res.status(200).json({msg: 'User created', token})
+}
+
+
+const dashboard = async (req, res) =>{
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token', 401)
+    }
+    const token = authHeader.split(' ')[1]
+    
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const luckyNumber = Math.floor(Math.random()*100)
+        res.status(200).json({msg:`Hello, ${decoded.username}`,secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route',401)
+    }
+
+}
+
+module.exports = {
+    login, dashboard
+}
